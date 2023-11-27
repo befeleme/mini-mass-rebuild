ac570619b13517cbd1aba517eaf655c2f82b8487
Karolina Surma <ksurma@redhat.com>
2023-11-27 16:37:41 +0100

    We build against Python 3.13.0~a2 now

--- monitor_check.py
+++ monitor_check.py
@@ -752,7 +752,7 @@ async def open_bz(package, build, status, browser_lock, reason=None):
     summary = f"{package} fails to build with Python 3.13: {reason['short_description']}"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.13.0a1.
+        {package} fails to build with Python 3.13.0a2.
 
         {reason['long_description']}
 
