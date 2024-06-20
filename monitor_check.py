d0a57ad7e19df9afd23b362600f8f7076776c216
Karolina Surma <ksurma@redhat.com>
2024-06-20 14:10:48 +0200

    File the bugzillas for b2 now

--- monitor_check.py
+++ monitor_check.py
@@ -754,7 +754,7 @@ async def open_bz(package, build, status, browser_lock, reason=None):
     summary = f"{package} fails to build with Python 3.13: {reason['short_description']}"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.13.0b1.
+        {package} fails to build with Python 3.13.0b2.
 
         {reason['long_description']}
 
