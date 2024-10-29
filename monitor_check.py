9568e67a80ad77d317a5add04048e407b311b635
Karolina Surma <ksurma@redhat.com>
2024-10-29 12:28:21 +0100

    Prepare for Python 3.14

--- monitor_check.py
+++ monitor_check.py
@@ -17,7 +17,7 @@ from anytree import Node, RenderTree, findall_by_attr, LoopError
 
 from copr.v3 import Client
 
-COPR = '@python', 'python3.13'
+COPR = '@python', 'python3.14'
 COPR_STR = '{}/{}'.format(*COPR)
 COPR_STR_G = '{}/{}'.format(COPR[0].replace('@', 'g/'), COPR[1])
 
@@ -28,14 +28,14 @@ PACKAGE = re.compile(fr'<a href="/coprs/{COPR_STR_G}/package/([^/]+)/">')
 BUILD = re.compile(fr'<a href="/coprs/{COPR_STR_G}/build/([^/]+)/">')
 RESULT = re.compile(r'<span class="build-([^"]+)"')
 RPM_FILE = "<td class='t'>RPM File</td>"
-TAG = 'f40'
+TAG = 'f42'
 # copr bug: build.log isn't properly populated
 # TODO: rework to use builder-live.log.gz or wait for https://github.com/fedora-copr/copr/issues/2961
 LIMIT = 30
 BUGZILLA = 'bugzilla.redhat.com'
 BZ_PAGE_SIZE = 20
-TRACKER = 2244836  # PYTHON3.13
-RAWHIDE = 2260875  # F41FTBFS
+TRACKER = 2322407  # PYTHON3.14
+RAWHIDE = 2300528  # F42FTBFS
 LOGLEVEL = logging.WARNING
 
 DNF_CACHEDIR = '_dnf_cache_dir'
@@ -57,266 +57,6 @@ EXCLUDE = {
 }
 
 REASONS = {
-    "implicit declaration of PyEval_InitThreads": {
-        "regex": r"(.*error:.*(PyEval_InitThreads|PyEval_ThreadsInitialized).*)",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Remove PyEval_InitThreads() and PyEval_ThreadsInitialized() functions, deprecated in Python 3.9.
-        Since Python 3.7, Py_Initialize() always creates the GIL:
-        calling PyEval_InitThreads() did nothing and PyEval_ThreadsInitialized() always returned non-zero.
-        (Contributed by Victor Stinner in https://github.com/python/cpython/issues/105182.)
-        """,
-        "short_description": "error: implicit declaration of function ‘PyEval_InitThreads’",
-    },
-    "_PyThreadState_UncheckedGet is now public": {
-        "regex": r"(.*error:.*_PyThreadState_UncheckedGet.*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html a new public function has been added:
-
-        Add PyThreadState_GetUnchecked() function: similar to PyThreadState_Get(), but don’t kill the process with a fatal error if it is NULL.
-        The caller is responsible to check if the result is NULL.
-        Previously, the function was private and known as _PyThreadState_UncheckedGet().
-        (Contributed by Victor Stinner in gh-108867.)
-        """,
-        "short_description": "",
-    },
-    "time.h-sys/select.h-sys/time.h": {
-        "regex": r"(.*error:.*(gettimeofday|clock|gmtime|select|futimes|setitimer).*)",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Python.h no longer includes these standard header files: <time.h>, <sys/select.h> and <sys/time.h>.
-        If needed, they should now be included explicitly.
-        For example, <time.h> provides the clock() and gmtime() functions, <sys/select.h> provides the select() function,
-        and <sys/time.h> provides the futimes(), gettimeofday() and setitimer() functions.
-        (Contributed by Victor Stinner in gh-108765.)
-        """,
-        "short_description": "error: Missing declaration of <time.h>, <sys/select.h> or <sys/time.h> module",
-    },
-    "unistd.h": {
-        "regex": r"(.*error:.*(getpid|read|write|close|isatty|lseek|getpid|getcwd|sysconf|getpagesize|dup|access).*)",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Python.h no longer includes the <unistd.h> standard header file.
-        If needed, it should now be included explicitly.
-        For example, it provides the functions: read(), write(), close(), isatty(), lseek(), getpid(), getcwd(), sysconf() and getpagesize().
-        As a consequence, _POSIX_SEMAPHORES and _POSIX_THREADS macros are no longer defined by Python.h.
-        The HAVE_UNISTD_H and HAVE_PTHREAD_H macros defined by Python.h can be used to decide if <unistd.h> and <pthread.h> header files can be included.
-        (Contributed by Victor Stinner in gh-108765.)
-        """,
-        "short_description": "Missing <unistd.h> declaration",
-    },
-    "_PyLong_AsByteArray or _PyLong_FromByteArray or _Py_IDENTIFIER": {
-        "regex": r"(.*error:.*(_PyLong_AsByteArray|_PyLong_FromByteArray|_Py_IDENTIFIER|_PyLong_New).*)",
-        "long_description": """{MATCH}
-
-        This function has been removed from Python 3.13.
-        The detailed list of the removed private C API functions can be found here:
-        https://github.com/python/cpython/issues/106320
-        """,
-        "short_description": "",
-    },
-    "initialization functions": {
-        "regex": r"(.*error:.*(Py_SetProgramName|Py_SetPythonHome|Py_SetStandardStreamEncoding|_Py_SetProgramFullPath|Py_SetPath|PySys_SetPath|PySys_SetArgv|PySys_SetArgvEx|PySys_HasWarnOptions|PySys_AddXOption|PySys_AddWarnOption|PySys_AddWarnOptionUnicode).*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-        Remove the following old functions to configure the Python initialization, deprecated in Python 3.11:
-            PySys_AddWarnOptionUnicode(): use PyConfig.warnoptions instead.
-            PySys_AddWarnOption(): use PyConfig.warnoptions instead.
-            PySys_AddXOption(): use PyConfig.xoptions instead.
-            PySys_HasWarnOptions(): use PyConfig.xoptions instead.
-            PySys_SetArgvEx(): set PyConfig.argv instead.
-            PySys_SetArgv(): set PyConfig.argv instead.
-            PySys_SetPath(): set PyConfig.module_search_paths instead.
-            Py_SetPath(): set PyConfig.module_search_paths instead.
-            Py_SetProgramName(): set PyConfig.program_name instead.
-            Py_SetPythonHome(): set PyConfig.home instead.
-            Py_SetStandardStreamEncoding(): set PyConfig.stdio_encoding instead, and set also maybe PyConfig.legacy_windows_stdio (on Windows).
-            _Py_SetProgramFullPath(): set PyConfig.executable instead.
-        """,
-        "short_description": "error: implicit declaration of function XXX",
-    },
-    "Py_TRASHCAN_SAFE": {
-        "regex": r"(.*error: .*(Py_TRASHCAN_SAFE_BEGIN|Py_TRASHCAN_SAFE_END).*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-        Remove the old trashcan macros Py_TRASHCAN_SAFE_BEGIN and Py_TRASHCAN_SAFE_END.
-        They should be replaced by the new macros Py_TRASHCAN_BEGIN and Py_TRASHCAN_END.
-        The new macros were added in Python 3.8 and the old macros were deprecated in Python 3.11.
-        (Contributed by Irit Katriel in gh-105111.)
-        """,
-        "short_description": "",
-    },
-    "importlib.resources": {
-        "regex": r"(ImportError: cannot import name '(.*?)' from 'importlib.resources'|AttributeError: module 'importlib.resources' has no attribute '(.*?)')",
-        "long_description": """{MATCH}
-
-        The deprecated importlib.resources methods were removed from Python 3.13:
-        - contents()
-        - is_resource()
-        - open_binary()
-        - open_text()
-        - path()
-        - read_binary()
-        - read_text()
-        Use files() instead. Refer to importlib-resources: Migrating from Legacy for migration advice.
-        https://importlib-resources.readthedocs.io/en/latest/using.html#migrating-from-legacy
-        """,
-        "short_description": "",
-    },
-    "telnetlib": {
-        "regex": r"(ModuleNotFoundError: No module named 'telnetlib')",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-        PEP 594: Remove the telnetlib module, deprecated in Python 3.11: use the projects telnetlib3 or Exscript instead.
-        (Contributed by Victor Stinner in gh-104773.)
-        """,
-        "short_description": "",
-    },
-    "crypt": {
-        "regex": r"(ModuleNotFoundError: No module named 'crypt')",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        PEP 594: Remove the crypt module and its private _crypt extension, deprecated in Python 3.11.
-        The hashlib module is a potential replacement for certain use cases.
-        Otherwise, the following PyPI projects can be used:
-        - bcrypt: Modern password hashing for your software and your servers.
-        - passlib: Comprehensive password hashing framework supporting over 30 schemes.
-        - argon2-cffi: The secure Argon2 password hashing algorithm.
-        - legacycrypt: Wrapper to the POSIX crypt library call and associated functionality.
-        (Contributed by Victor Stinner in gh-104773.)
-        """,
-        "short_description": "",
-    },
-    "locale.resetlocale": {
-        "regex": r"(ImportError: cannot import name 'resetlocale' from 'locale')",
-        "long_description": """{MATCH}
-
-        Remove locale.resetlocale() function deprecated in Python 3.11: use locale.setlocale(locale.LC_ALL, "") instead.
-        (Contributed by Victor Stinner in gh-104783.)
-        """,
-        "short_description": "",
-    },
-    "implicit declaration of PyEval_AcquireLock or PyEval_ReleaseLock": {
-        "regex": r"(.*error:.*(PyEval_AcquireLock|PyEval_ReleaseLock).*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Remove PyEval_AcquireLock() and PyEval_ReleaseLock() functions, deprecated in Python 3.2.
-        They didn’t update the current thread state. They can be replaced with:
-            - PyEval_SaveThread() and PyEval_RestoreThread();
-            - low-level PyEval_AcquireThread() and PyEval_RestoreThread();
-            - or PyGILState_Ensure() and PyGILState_Release().
-        (Contributed by Victor Stinner in gh-105182.)
-        """,
-        "short_description": "",
-    },
-    "implicit declaration of PyEval_CallObject or PyEval_CallObjectWithKeywords": {
-        "regex": r"(.*error:.*(PyEval_CallObject|PyEval_CallObjectWithKeywords).*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Remove PyEval_CallObject(), PyEval_CallObjectWithKeywords(): use PyObject_CallNoArgs() or PyObject_Call() instead.
-        Warning: PyObject_Call() positional arguments must be a tuple and must not be NULL,
-        keyword arguments must be a dict or NULL, whereas removed functions checked arguments type and accepted NULL positional and keyword arguments.
-        To replace PyEval_CallObjectWithKeywords(func, NULL, kwargs) with PyObject_Call(),
-        pass an empty tuple as positional arguments using PyTuple_New(0).
-        """,
-        "short_description": "",
-    },
-    "ctype.h": {
-        "regex": r"(.*include.*ctype\.h.*)",
-        "long_description": """{MATCH}
-
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Python.h no longer includes the <ctype.h> standard header file.
-        If needed, it should now be included explicitly.
-        For example, it provides isalpha() and tolower() functions which are locale dependent.
-        Python provides locale independent functions, like Py_ISALPHA() and Py_TOLOWER().
-        (Contributed by Victor Stinner in gh-108765.)
-        """,
-        "short_description": "Missing <ctype.h> declaration",
-    },
-    "no cgi": {
-        "regex": r"(ModuleNotFoundError: No module named \'cgi\')",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html module cgi was removed:
-
-        PEP 594: Remove the cgi and cgitb modules, deprecated in Python 3.11.
-
-        cgi.FieldStorage can typically be replaced with urllib.parse.parse_qsl() for GET and HEAD requests, and the email.message module or multipart PyPI project for POST and PUT.
-
-        cgi.parse() can be replaced by calling urllib.parse.parse_qs() directly on the desired query string, except for multipart/form-data input, which can be handled as described for cgi.parse_multipart().
-
-        cgi.parse_multipart() can be replaced with the functionality in the email package (e.g. email.message.EmailMessage and email.message.Message) which implements the same MIME RFCs, or with the multipart PyPI project.
-
-        cgi.parse_header() can be replaced with the functionality in the email package, which implements the same MIME RFCs. For example, with email.message.EmailMessage:
-
-        from email.message import EmailMessage
-        msg = EmailMessage()
-        msg['content-type'] = 'application/json; charset="utf8"'
-        main, params = msg.get_content_type(), msg['content-type'].params
-
-        (Contributed by Victor Stinner in gh-104773.)
-        """,
-        "short_description": "ModuleNotFoundError: No module named 'cgi'",
-    },
-    "no pipes": {
-        "regex": r"(ModuleNotFoundError: No module named \'pipes\')",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html module pipes was removed:
-
-        PEP 594: Remove the pipes module, deprecated in Python 3.11: use the subprocess module instead.
-        (Contributed by Victor Stinner in gh-104773.)
-        """,
-        "short_description": "ModuleNotFoundError: No module named 'pipes'",
-    },
-    "no tkinter.tix": {
-        "regex": r"(ModuleNotFoundError: No module named \'tkinter\.tix\')",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Remove the tkinter.tix module, deprecated in Python 3.6.
-        The third-party Tix library which the module wrapped is unmaintained.
-        (Contributed by Zachary Ware in gh-75552.)
-        """,
-        "short_description": "ModuleNotFoundError: No module named 'tkinter.tix'",
-    },
-    "'Logger' object has no attribute 'warn'": {
-        "regex": r"(\'Logger\' object has no attribute \'warn\'|AttributeError: module 'logging' has no attribute 'warn')",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        logging: Remove undocumented and untested Logger.warn() and LoggerAdapter.warn() methods and logging.warn() function.
-        Deprecated since Python 3.3, they were aliases to the logging.Logger.warning() method,
-        logging.LoggerAdapter.warning() method and logging.warning() function.
-        (Contributed by Victor Stinner in gh-105376.)
-        """,
-        "short_description": "",
-    },
-    "unittest": {
-        "regex": r"(AttributeError: module \'unittest\' has no attribute \'(makeSuite|findTestCases|getTestCaseNames)\')",
-        "long_description": """{MATCH}
-        According to https://docs.python.org/3.13/whatsnew/3.13.html:
-
-        Removed the following unittest functions, deprecated in Python 3.11:
-        - unittest.findTestCases()
-        - unittest.makeSuite()
-        - unittest.getTestCaseNames()
-        """,
-        "short_description": "AttributeError: module 'unittest' has no attribute XXX",
-    },
     "segfault": {
         # Segfault detection is quite noisy, especially if we do not want to report it this way. I temporarily disabled it with X in regex.
         "regex": r"XSegmentation fault",
@@ -331,7 +71,7 @@ BZAPI = bugzilla.Bugzilla(BUGZILLA)
 
 def _bugzillas():
     query = BZAPI.build_query(product='Fedora')
-    query['blocks'] = [TRACKER, RAWHIDE]
+    query['blocks'] = [TRACKER]  #, RAWHIDE]
     query['limit'] = BZ_PAGE_SIZE
     query['offset'] = 0
     results = []
@@ -751,29 +491,29 @@ async def open_bz(package, build, status, browser_lock, reason=None):
             "long_description": "This report is automated and not very verbose, but we'll try to get back here with details.",
             "short_description": "",
         }
-    summary = f"{package} fails to build with Python 3.13: {reason['short_description']}"
+    summary = f"{package} fails to build with Python 3.14: {reason['short_description']}"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.13.0b2.
+        {package} fails to build with Python 3.14.0a1.
 
         {reason['long_description']}
 
-        https://docs.python.org/3.13/whatsnew/3.13.html
+        https://docs.python.org/3.14/whatsnew/3.14.html
 
         For the build logs, see:
         https://copr-be.cloud.fedoraproject.org/results/{COPR_STR}/fedora-rawhide-x86_64/{build:08}-{package}/
 
-        For all our attempts to build {package} with Python 3.13, see:
+        For all our attempts to build {package} with Python 3.14, see:
         https://copr.fedorainfracloud.org/coprs/{COPR_STR_G}/package/{package}/
 
         Testing and mass rebuild of packages is happening in copr.
-        You can follow these instructions to test locally in mock if your package builds with Python 3.13:
+        You can follow these instructions to test locally in mock if your package builds with Python 3.14:
         https://copr.fedorainfracloud.org/coprs/{COPR_STR_G}/
 
         Let us know here if you have any questions.
 
-        Python 3.13 is planned to be included in Fedora 41.
-        To make that update smoother, we're building Fedora packages with all pre-releases of Python 3.13.
+        Python 3.14 is planned to be included in Fedora 43.
+        To make that update smoother, we're building Fedora packages with all pre-releases of Python 3.14.
         A build failure prevents us from testing all dependent packages (transitive [Build]Requires),
         so if this package is required a lot, it's important for us to get it fixed soon.
 
