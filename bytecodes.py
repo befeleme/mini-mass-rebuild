9f25340386939fc4f445a4b63bbac7bd50d26656
Karolina Surma <ksurma@redhat.com>
2025-09-22 12:57:08 +0200

    Update bytecodes script and output for rc3

--- bytecodes.py
+++ bytecodes.py
@@ -2,7 +2,7 @@ import json
 import subprocess
 from click import progressbar
 
-repoquery = 'repoquery --repo=koji43 --refresh -f *.cpython-314.pyc --source'.split()
+repoquery = 'repoquery --repo=koji43 --repo=updates-testing --refresh -f *.cpython-314.pyc --latest=1 --source'.split()
 py314_pkgs = subprocess.run(repoquery, stdout=subprocess.PIPE, text=True).stdout.splitlines()
 
 try:
@@ -41,14 +41,12 @@ try:
             name = '-'.join(nevr.split('-')[:-2])
             if name in processed:
                 continue
-            if nevr not in after(name, '2025-08-15 23:59:59'):
+            if nevr not in after(name, '2025-09-19 23:59:59'):
                 # https://koji.fedoraproject.org/koji/buildinfo?buildID=2791425
                 # https://bodhi.fedoraproject.org/updates/FEDORA-2025-2fe07c73d5
                 # https://bodhi.fedoraproject.org/overrides/python3.14-3.14.0~rc2-1.fc43
-                if nevr not in after(name, '2025-08-15 12:39:34'):
+                if nevr not in after(name, '2025-09-19 09:39:00'):
                     torebuild.add(name)
-                elif built_by(nevr, 'churchyard'):
-                    done.add(name)
                 else:
                     inspection.add(name)
             else:
@@ -58,9 +56,9 @@ except KeyboardInterrupt:
     print('Interrupted.\n')
 
 print(f'Processed {len(processed)} packages.\n')
-print(f'{len(done)} packages were build with rc2+')
-print(f'{len(inspection)} packages were built on 2025-06-18 and need manual inspection')
-print(f'{len(torebuild)} packages need to be rebuilt with rc2+')
+print(f'{len(done)} packages were build with rc3+')
+print(f'{len(inspection)} packages were built on 2025-09-19 and need manual inspection')
+print(f'{len(torebuild)} packages need to be rebuilt with rc3+')
 
 with open('bytecodes.json', 'w') as f:
     json.dump({'done': sorted(done),
